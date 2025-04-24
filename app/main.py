b7ac8b9d4bf5ffc22560285ff1e2d75585965486
Valentyn Pyzhyk <pyzhyk.business@gmail.com>
2025-04-24 10:27:14 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -7,15 +7,20 @@ class Animal:
         self.hidden = False
         Animal.alive.append(self)
 
+    def take_damage(self, amount: int) -> None:
+        self.health = max(self.health - amount, 0)
+        if self.health == 0:
+            self.die()
+
+    def die(self) -> None:
+        if self in self.__class__.alive:
+            Animal.alive.remove(self)
+
     def __repr__(self) -> str:
         return (f"{{"
                 f"Name: {self.name}, "
                 f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
-
-    def die(self) -> None:
-        if self in Animal.alive:
-            Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
@@ -26,6 +31,4 @@ class Herbivore(Animal):
 class Carnivore(Animal):
     def bite(self, other: Animal) -> None:
         if isinstance(other, Herbivore) and not other.hidden:
-            other.health -= 50
-            if other.health <= 0:
-                other.die()
+            other.take_damage(50)
